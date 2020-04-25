d7a2d0333599b104f5184ca836240f3aabc331e0
lqh-gzhu <61220328+lqh-gzhu@users.noreply.github.com>
2020-04-25 16:38:03 +0800

    创建目标2.1.1

--- pages/createTarget/createTarget.js
+++ pages/createTarget/createTarget.js
@@ -7,9 +7,9 @@ Page({
    */
   data: {
    numbers:[50,100,200,600,800,1000],
-   change:true,
+   change:false,
    change_1:true,
-   change_2:false,
+   change_2:true,
    custom:true,
    label:'',
    setCoin:'',
