8ffa9cbcf48282fe2341896b0e4647f42739fe6b
lqh-gzhu <61220328+lqh-gzhu@users.noreply.github.com>
2020-04-27 22:09:22 +0800

    创建目标新布局_1

--- pages/createTarget/createTarget.js
+++ pages/createTarget/createTarget.js
@@ -7,11 +7,11 @@ Page({
    */
   data: {
    numbers:[50,100,200,600,800,1000],
-   change:true,
+   change:false,
    change_1:true,
    change_2:true,
    custom:true,
-   showtitle:false,
+   showtitle:true,
    showCircle:true,
    showCircle_1:true,showCircle_2:true,showCircle_3:true,showCircle_4:true,
    showCircle_5:true,showCircle_6:true,showCircle_7:true,showCircle_8:true,
