7d9e253c4319b45b4232c428117afe6478b64840
lqh-gzhu <61220328+lqh-gzhu@users.noreply.github.com>
2020-05-09 20:45:29 +0800

    一些小细节

---
+++ cloudfunctions/setCoin/index.js
@@ -0,0 +1,28 @@
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+ console.log(event.friendName)
+ const db = cloud.database();
+  const _ = db.command;
+  const wxContext = cloud.getWXContext();
+  let userId = wxContext.OPENID;
+  let curCoin=event.curCoin
+  console.log("curCoin",curCoin)
+  try {
+    return   await db.collection('user')
+    .where({
+      _openid:_.eq(userId)
+    })
+    .update({
+      data:{
+        coin:curCoin
+      }
+    })
+  } catch (e) {
+    console.error("err",e)
+  }
+  // collectio
+}
