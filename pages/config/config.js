06d835a7911417e2b8d2844019b4ed82d35ae159
lqh-gzhu <61220328+lqh-gzhu@users.noreply.github.com>
2020-05-11 16:30:59 +0800

    修改设置页面以及其他一些小问题

--- pages/config/config.js
+++ pages/config/config.js
@@ -9,6 +9,7 @@ Page({
     content:'',//用户自己的评论
     myself:'',//用户OPENid
     nowdate:'',
+
     //加载表示符，用于控制加载动画,当值为 "" 隐藏
     loadContent: "加载中...",
     //通知窗口表示符，用于控制加载动画,当值为 "" 隐藏
@@ -18,6 +19,27 @@ Page({
     //当前主题
     changeView:"",
     Towhere:-1, //为0时跳转到白色界面，为1时跳转到绿色界面
+    showIdea:true,
+    problem:[  {title:"闪退/卡顿",
+    specific:"页面闪退或卡顿"},
+    {title:"目标发布",
+    specific:"发布失败，内容丢失"},
+    {title:"商城/支付",
+    specific:"商品加载，支付失败"},
+    {title:"进度/时间",
+    specific:"无法刷新，不显示"},
+    {title:"围观/投币",
+    specific:"无法围观，无法投币"},
+    {title:"私信/评论",
+    specific:"关注，发送接受私信"},
+    {title:"个人中心",
+    specific:"账号注销，资料不显示"},
+    {title:"其他",
+    specific:"点赞，历史进程"},
+  ],
+    title:'',//存放点击的错误的标题
+    inputProblem:true,
+    problemContent:''
   },
   backHome: function () {
    wx.navigateBack({
@@ -25,12 +47,69 @@ Page({
     })
   },
 
+  choose:function(e){
+    let index = e.currentTarget.id;
+    console.log(index)
+    this.setData({
+        title:this.data.problem[index].title,
+        inputProblem:false
+      })
+  },
+  problem_back:function(){
+   this.setData({
+     showIdea:true
+   })
+  },
+  getInputProblem:function(e){
+    this.setData({
+      problemContent: e.detail.value
+    })
+  },
+  problem_return:function(){
+     this.setData({
+       inputProblem:true,
+       input:''
+     })
+  },
+  problem_submit:function(){
+    let date=new Date()
+    this.setData({
+      nowdate:date
+    })
+    if(this.data.problemContent==''){
+      this.setData({
+        informContent:"请输入有效意见~"
+      })
+      return
+    }
+      const db=wx.cloud.database()
+      let title=this.data.title
+      let problemContent=this.data.problemContent
+      db.collection('suggestion')
+      .add({
+         data: {
+      content:problemContent,
+      category:title,
+      time:this.data.nowdate}
+        })
+        .then(res=>{
+             console.log("提交成功",res)
+             this.setData({
+             informContent:'已经提交',
+             inputProblem:true,
+         })
+     })
+  },
   changeview:function(e){  //点击白色
     let theme = e.currentTarget.dataset.theme;
     console.log(theme);
     this.setData({ changeView:theme});
   },
-
+  showIdea:function(){
+    this.setData({
+      showIdea:false
+    })
+  },
   toChooseTheme:function(){
     //根据主题跳转
     let url = "";
@@ -94,7 +173,6 @@ Page({
     let content=this.data.content
     console.log(content)
     let time=this.data.nowdate
- 
     const db=wx.cloud.database()
     db.collection('suggestion')
     .add({
